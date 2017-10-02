7d84d8fe262a81fb75f4c3fccbc75f1a5409fc36
Francis <francis.menkes@gmail.com>
2017-10-02 16:01:45 -0300

    More provisioning for vagrant box

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,9 @@
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 apt-get install -y nodejs
+sudo npm install -g nodemon
 sudo npm install -g n
 sudo n latest
 sudo apt-get -y remove nodejs
 sudo ln -s /usr/bin/node /usr/bin/nodejs
+cd /vagrant && npm install
+npm run dev
